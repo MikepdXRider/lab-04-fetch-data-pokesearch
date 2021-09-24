fb18dd0933f524be935fdc0ebde8d83dd006f51b
Michael Rider <mikepdxrider@gmail.com>
2021-09-23 17:13:24 -0700

    adds loading gif and fixes sort

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -9,12 +9,25 @@ export default class PokeList extends Component {
                 <ul className="poke-list">
                     {
                         isLoading
-                        ? <h1>Loading...</h1>
-                        : 
-                        dataArr.map((dataObj => {
+                        ? <img src='https://img.pikbest.com/58pic/35/39/61/62K58PICb88i68HEwVnm5_PIC2018.gif!w340' alt='loading...'/>
+                        : dataArr.map((dataObj => {
                             return(
-                                <li key={dataObj.id}>
+                                <li style={{backgroundColor: `${dataObj.color_1}`}} key={dataObj.id} className="poke-item">
+                                    <h2>{dataObj.pokemon}</h2>
+                                    <p><span>Type:</span> {dataObj.type_1}</p>
                                     <img src={dataObj.url_image} alt={dataObj.id} />
+                                    <section className='poke-item-info flex-cont'>
+                                        <div>
+                                            <p>HP: {dataObj.hp}</p>
+                                            <p>Ability: {dataObj.ability_1}</p>
+                                            <p>Hidden-Ability: {dataObj.ability_hidden}</p>
+                                        </div>
+                                        <div>
+                                            <p>Attack: {dataObj.attack}</p>
+                                            <p>Speed: {dataObj.speed}</p>
+                                            <p>Defence: {dataObj.defense}</p>
+                                        </div>
+                                    </section>
                                 </li>
                             )
                         }))
--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PokeList from './PokeList.js'
 import request from 'superagent'
+import sortData from '../sortData.js'
 
 
 
@@ -8,6 +9,7 @@ export default class SearchPage extends Component {
     state = {
         dataArr: [],
         searchQuery: '',
+        sortBy: '',
         searchOrder: 'asc',
         isLoading: false
     }
@@ -16,8 +18,8 @@ export default class SearchPage extends Component {
     handleClick = () => {
         const searchQuery = this.state.searchQuery
         const searchOrder = this.state.searchOrder
-
-        this.fetchSearch(searchQuery, searchOrder)
+        const sortBy = this.state.sortBy
+        this.fetchSearch(sortBy, searchOrder, searchQuery)
     }
 
 
@@ -27,6 +29,14 @@ export default class SearchPage extends Component {
         // updates searchOrder state to e.target.value
         this.setState({searchQuery: e.target.value})
         console.log(this.state.searchQuery)
+    }
+
+
+    // handleSearch Method
+    // takes in e
+    handleSort = (e) =>{
+        // updates searchOrder state to e.target.value
+        this.setState({sortBy: e.target.value})
     }
     
     
@@ -42,43 +52,50 @@ export default class SearchPage extends Component {
     componentDidMount = () => {
         const searchQuery = this.state.searchQuery
         const searchOrder = this.state.searchOrder
-
-        this.fetchSearch(searchQuery, searchOrder)
+        const sortBy = this.state.sortBy
+        this.fetchSearch(sortBy, searchOrder, searchQuery)
     }
 
 
     // fetchSearch async function
     // takes in searchQuery and searchOrder
-    fetchSearch = async (searchQuery, searchOrder) => {
+    fetchSearch = async (sortBy , searchOrder, searchQuery) => {
         // updates isLoading state to true
         this.setState({isLoading: true})
 
-        const response = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}&direction=${this.state.searchOrder}`)
+        const response = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${searchQuery}&sort=${sortBy}&direction=${searchOrder}&`)
             
         // update dataArr state
         this.setState({dataArr: [...response.body.results]})
         // update isLoading state to false
         this.setState({isLoading: false})
 
-        console.log('isLoading state leaving function', this.state.isLoading)
-        console.log('dataArr state leaving function', this.state.dataArr)
-        console.log('searchOrder state leaving function', this.state.searchOrder)
+        // console.log('isLoading state leaving function', this.state.isLoading)
+        // console.log('dataArr state leaving function', this.state.dataArr)
+        // console.log('searchOrder state leaving function', this.state.searchOrder)
     }
-
-
 
 
     render() {
         return (
             <main>
                 <section>
-                    <input type='text' onChange={this.handleSearch} />
-                    <button onClick={this.handleClick}>Search</button>
-                    {/* onChange = handleSearchOrder method */}
+
+                    <div className="search-cont"> 
+                        <input type='text' onChange={this.handleSearch} />
+                        <button onClick={this.handleClick}>Search</button>
+                    </div>
+                        <span>Sort:</span>  
+                   <select onChange={this.handleSort}>
+                        {
+                            sortData.map((sortItem) => <option key={sortItem} value={sortItem}>{sortItem.toLocaleUpperCase()}</option>)
+                        }
+                    </select>
                     <select onChange={this.handleSearchOrder}>
                         <option key='asc' value='asc'>Ascending</option>
                         <option key='desc' value='desc'>Descending</option>
                     </select>
+
                 </section>
                 <PokeList
                 dataArr = {this.state.dataArr} 
