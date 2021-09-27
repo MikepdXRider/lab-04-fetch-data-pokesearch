a0fbe788177d9792b3a7c69978f1609222b9de75
Michael Rider <mikepdxrider@gmail.com>
2021-09-27 13:27:40 -0700

    adds react-router-dom and creates home and details page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link,
+  // useParams
+} from "react-router-dom";
 import SearchPage from './components/SearchPage.js'
+import HomePage from './components/HomePage.js';
+import DetailsPage from './components/DetailsPage.js';
 
-function App() {
-  return (
-    <div className="App">
-      <SearchPage />
-    </div>
-  );
+// Params are placeholders in the URL that begin
+// with a colon, like the `:id` param defined in
+// the route in this example. A similar convention
+// is used for matching dynamic segments in other
+// popular web frameworks like Rails and Express.
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route 
+              path="/search-page" 
+              exact
+              render={(routerProps) => <SearchPage {...routerProps} />} 
+            />          
+            <Route 
+              path="/" 
+              exact
+              render={(routerProps) => <HomePage {...routerProps} />} 
+            />          
+            <Route 
+              path="/details-page:query" 
+              exact
+              render={(routerProps) => <DetailsPage {...routerProps} />} 
+            />          
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+// class Child extends React.Component {
+//   render() {
+
+//     return (
+//       <div>
+//         <h3>ID: {props.match.params.myId}</h3>
+//       </div>
+//     );
+//   }
+// }
---
+++ src/components/DetailsPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DetailsPage extends Component {
+    render() {
+        return (
+            <header>
+                <h1>Welcome to details...</h1>
+            </header>
+        )
+    }
+}
